ed3edf9245499cc40cc3e375bf76ebe52a2a05bd
edivaldogundim <edivaldogundim@gmail.com>
2021-02-05 01:41:59 -0300

    React Redux - back

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Menu from './components/menu';
 
 function App() {
+const [inputFrutas, setInputFrutas] = React.useState("");
+const [inputTitulo, setInputTitulo] = React.useState("")
+
+  const frutas = useSelector((state) => state.frutasReducer.frutas)
+  const stateTitulo = useSelector((state) => state.tituloReducer.titulo)
+  
+  const dispatch = useDispatch();
+  
+  function adicionarFruta(event){
+    event.preventDefault();
+
+    const objFruta = {
+      nome: inputFrutas
+    }
+    dispatch({type: "ADICIONAR", value: objFruta});
+  }
+
+  function alterarTitulo(event){
+    setInputTitulo(event.target.value)
+    dispatch({type: "ALTERAR", value: event.target.value});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+
+      
+    <form>
+      <label>Título</label>
+      <input placeholder="Digite um título" 
+      value={inputTitulo} 
+      onChange={alterarTitulo}/>
+    </form>
+
+    <h1>{stateTitulo}</h1>
+      
+      <form onSubmit={adicionarFruta}>
+      <input 
+      placeholder="Digite o nome de uma fruta" 
+      value={inputFrutas}
+      onChange={(event) => setInputFrutas(event.target.value)} />
+      <button>
+        Enviar
+      </button>
+
+      </form>
+
+      
+
+    {frutas.map((frutas, index) => {
+      return (
+        <li key={index}>{frutas.nome}</li>
+      )
+    })}
     </div>
   );
 }
